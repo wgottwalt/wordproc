c7d8f5e17492b2e29cb99e7f13669cb0017c1eb0
Wilken 'Akiko' Gottwalt <12194808+wgottwalt@users.noreply.github.com>
2021-04-29 14:21:07 +0200

    updated i18n files to currently used strings

--- sources/i18n_Dutch.ts
+++ sources/i18n_Dutch.ts
@@ -1,4 +1,11 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="nl_NL">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>I18N_MAINWINDOW</source>
+        <translation>WordProc</translation>
+    </message>
+</context>
 </TS>
