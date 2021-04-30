61233e939bcd58cb9024ab2554737c201a1c4ec1
Wilken 'Akiko' Gottwalt <12194808+wgottwalt@users.noreply.github.com>
2021-04-30 14:08:29 +0200

    updated i18n files to currently used strings

--- sources/i18n_French.ts
+++ sources/i18n_French.ts
@@ -7,5 +7,177 @@
         <source>I18N_MAINWINDOW</source>
         <translation>WordProc</translation>
     </message>
+    <message>
+        <source>I18N_MM_APP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_MM_EDIT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_MM_FORMAT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_SM_FORMAT_ALIGN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_MM_WINDOW</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_MM_HELP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_MM_SEARCH</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_MM_CONFIG</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_SM_CONFIG_LANG</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_APP_NEW</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_APP_OPEN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_APP_SAVE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_APP_SAVEAS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_APP_CLOSE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_APP_PRINT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_APP_QUIT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_EDIT_UNDO</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_EDIT_REDO</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_EDIT_CUT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_EDIT_COPY</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_EDIT_PASTE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_EDIT_CLEAR</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_EDIT_SELECTALL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_SEARCH_FIND</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_SEARCH_REPLACE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_SEARCH_GOTO</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_FORMAT_FONT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_ALIGN_LEFT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_ALIGN_CENTER</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_ALIGN_RIGHT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_ALIGN_JUSTIFY</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_WINDOW_NEXT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_WINDOW_PREV</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_WINDOW_CLOSE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_WINDOW_CLOSEALL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_HELP_APP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_HELP_QT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_LANG_ENGLISH</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_LANG_DUTCH</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_LANG_FRENCH</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_LANG_ITALIAN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_LANG_GERMAN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_LANG_SPANISH</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 </TS>
