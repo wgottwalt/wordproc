1b71c16e6e63f1de6820d0eddefb68100848338c
Wilken 'Akiko' Gottwalt <12194808+wgottwalt@users.noreply.github.com>
2021-05-05 14:44:26 +0200

    added full English translation to current i18n files

--- sources/i18n_English.ts
+++ sources/i18n_English.ts
@@ -9,203 +9,203 @@
     </message>
     <message>
         <source>I18N_MM_APP</source>
-        <translation type="unfinished"></translation>
+        <translation>File</translation>
     </message>
     <message>
         <source>I18N_MM_EDIT</source>
-        <translation type="unfinished"></translation>
+        <translation>Edit</translation>
     </message>
     <message>
         <source>I18N_MM_FORMAT</source>
-        <translation type="unfinished"></translation>
+        <translation>Format</translation>
     </message>
     <message>
         <source>I18N_SM_FORMAT_ALIGN</source>
-        <translation type="unfinished"></translation>
+        <translation>Alignment</translation>
     </message>
     <message>
         <source>I18N_MM_WINDOW</source>
-        <translation type="unfinished"></translation>
+        <translation>Window</translation>
     </message>
     <message>
         <source>I18N_MM_HELP</source>
-        <translation type="unfinished"></translation>
+        <translation>Help</translation>
     </message>
     <message>
         <source>I18N_MM_SEARCH</source>
-        <translation type="unfinished"></translation>
+        <translation>Search</translation>
     </message>
     <message>
         <source>I18N_MM_CONFIG</source>
-        <translation type="unfinished"></translation>
+        <translation>Preferences</translation>
     </message>
     <message>
         <source>I18N_SM_CONFIG_LANG</source>
-        <translation type="unfinished"></translation>
+        <translation>Language</translation>
     </message>
     <message>
         <source>I18N_ME_APP_NEW</source>
-        <translation type="unfinished"></translation>
+        <translation>New</translation>
     </message>
     <message>
         <source>I18N_ME_APP_OPEN</source>
-        <translation type="unfinished"></translation>
+        <translation>Open ...</translation>
     </message>
     <message>
         <source>I18N_ME_APP_SAVE</source>
-        <translation type="unfinished"></translation>
+        <translation>Save</translation>
     </message>
     <message>
         <source>I18N_ME_APP_SAVEAS</source>
-        <translation type="unfinished"></translation>
+        <translation>Save as ...</translation>
     </message>
     <message>
         <source>I18N_ME_APP_CLOSE</source>
-        <translation type="unfinished"></translation>
+        <translation>Close</translation>
     </message>
     <message>
         <source>I18N_ME_APP_PRINT</source>
-        <translation type="unfinished"></translation>
+        <translation>Print</translation>
     </message>
     <message>
         <source>I18N_ME_APP_QUIT</source>
-        <translation type="unfinished"></translation>
+        <translation>Quit</translation>
     </message>
     <message>
         <source>I18N_ME_EDIT_UNDO</source>
-        <translation type="unfinished"></translation>
+        <translation>Undo</translation>
     </message>
     <message>
         <source>I18N_ME_EDIT_REDO</source>
-        <translation type="unfinished"></translation>
+        <translation>Redo</translation>
     </message>
     <message>
         <source>I18N_ME_EDIT_CUT</source>
-        <translation type="unfinished"></translation>
+        <translation>Cut</translation>
     </message>
     <message>
         <source>I18N_ME_EDIT_COPY</source>
-        <translation type="unfinished"></translation>
+        <translation>Copy</translation>
     </message>
     <message>
         <source>I18N_ME_EDIT_PASTE</source>
-        <translation type="unfinished"></translation>
+        <translation>Paste</translation>
     </message>
     <message>
         <source>I18N_ME_EDIT_CLEAR</source>
-        <translation type="unfinished"></translation>
+        <translation>Clear</translation>
     </message>
     <message>
         <source>I18N_ME_EDIT_SELECTALL</source>
-        <translation type="unfinished"></translation>
+        <translation>Select all</translation>
     </message>
     <message>
         <source>I18N_ME_SEARCH_FIND</source>
-        <translation type="unfinished"></translation>
+        <translation>Find ...</translation>
     </message>
     <message>
         <source>I18N_ME_SEARCH_REPLACE</source>
-        <translation type="unfinished"></translation>
+        <translation>Replace ...</translation>
     </message>
     <message>
         <source>I18N_ME_SEARCH_GOTO</source>
-        <translation type="unfinished"></translation>
+        <translation>Goto ...</translation>
     </message>
     <message>
         <source>I18N_ME_FORMAT_FONT</source>
-        <translation type="unfinished"></translation>
+        <translation>Font</translation>
     </message>
     <message>
         <source>I18N_ME_ALIGN_LEFT</source>
-        <translation type="unfinished"></translation>
+        <translation>Left</translation>
     </message>
     <message>
         <source>I18N_ME_ALIGN_CENTER</source>
-        <translation type="unfinished"></translation>
+        <translation>Center</translation>
     </message>
     <message>
         <source>I18N_ME_ALIGN_RIGHT</source>
-        <translation type="unfinished"></translation>
+        <translation>Right</translation>
     </message>
     <message>
         <source>I18N_ME_ALIGN_JUSTIFY</source>
-        <translation type="unfinished"></translation>
+        <translation>Justify</translation>
     </message>
     <message>
         <source>I18N_ME_WINDOW_NEXT</source>
-        <translation type="unfinished"></translation>
+        <translation>Next</translation>
     </message>
     <message>
         <source>I18N_ME_WINDOW_PREV</source>
-        <translation type="unfinished"></translation>
+        <translation>Previous</translation>
     </message>
     <message>
         <source>I18N_ME_WINDOW_CLOSE</source>
-        <translation type="unfinished"></translation>
+        <translation>Close</translation>
     </message>
     <message>
         <source>I18N_ME_WINDOW_CLOSEALL</source>
-        <translation type="unfinished"></translation>
+        <translation>Close all</translation>
     </message>
     <message>
         <source>I18N_ME_HELP_APP</source>
-        <translation type="unfinished"></translation>
+        <translation>About WordProc ...</translation>
     </message>
     <message>
         <source>I18N_ME_HELP_QT</source>
-        <translation type="unfinished"></translation>
+        <translation>About Qt ...</translation>
     </message>
     <message>
         <source>I18N_ME_LANG_ENGLISH</source>
-        <translation type="unfinished"></translation>
+        <translation>English</translation>
     </message>
     <message>
         <source>I18N_ME_LANG_DUTCH</source>
-        <translation type="unfinished"></translation>
+        <translation>Dutch</translation>
     </message>
     <message>
         <source>I18N_ME_LANG_FRENCH</source>
-        <translation type="unfinished"></translation>
+        <translation>French</translation>
     </message>
     <message>
         <source>I18N_ME_LANG_ITALIAN</source>
-        <translation type="unfinished"></translation>
+        <translation>Italian</translation>
     </message>
     <message>
         <source>I18N_ME_LANG_GERMAN</source>
-        <translation type="unfinished"></translation>
+        <translation>German</translation>
     </message>
     <message>
         <source>I18N_ME_LANG_SPANISH</source>
-        <translation type="unfinished"></translation>
+        <translation>Spanish</translation>
     </message>
     <message>
         <source>I18N_SM_CONFIG_THEME</source>
-        <translation type="unfinished"></translation>
+        <translation>Theme</translation>
     </message>
     <message>
         <source>I18N_ME_THEME_DEFAULT</source>
-        <translation type="unfinished"></translation>
+        <translation>Default Theme (Fusion)</translation>
     </message>
     <message>
         <source>I18N_ME_THEME_FUSION</source>
-        <translation type="unfinished"></translation>
+        <translation>Fusion</translation>
     </message>
     <message>
         <source>I18N_ME_THEME_WINDOWS</source>
-        <translation type="unfinished"></translation>
+        <translation>Windows</translation>
     </message>
     <message>
         <source>I18N_ME_THEME_MACOS</source>
-        <translation type="unfinished"></translation>
+        <translation>MacOS</translation>
     </message>
     <message>
         <source>I18N_ME_THEME_QTCURVE</source>
-        <translation type="unfinished"></translation>
+        <translation>QtCurve</translation>
     </message>
     <message>
         <source>I18N_ME_THEME_DARKMODE</source>
-        <translation type="unfinished"></translation>
+        <translation>Dark mode</translation>
     </message>
 </context>
 </TS>
