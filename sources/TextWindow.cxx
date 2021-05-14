e177c6e76d7834f5a99663c7cc2e1ee90c81831c
Wilken 'Akiko' Gottwalt <12194808+wgottwalt@users.noreply.github.com>
2021-05-14 11:39:01 +0200

    updated TextWindow class
    Added some toolbar functionality and fixed missing UI creation.

--- sources/TextWindow.cxx
+++ sources/TextWindow.cxx
@@ -5,10 +5,67 @@
 TextWindow::TextWindow(QWidget *parent)
 : QWidget(parent), Ui::TextWindow()
 {
+    setupUi(this);
 }
 
 TextWindow::~TextWindow()
 {
+}
+
+//--- public methods ---
+
+void TextWindow::formatText(const Types::Format format)
+{
+    switch (format)
+    {
+        case Types::Format::Undo:
+            wid_text->undo();
+            return;
+
+        case Types::Format::Redo:
+            wid_text->redo();
+            return;
+
+        case Types::Format::Cut:
+            wid_text->cut();
+            return;
+
+        case Types::Format::Copy:
+            wid_text->copy();
+            return;
+
+        case Types::Format::Paste:
+            wid_text->paste();
+            return;
+
+        case Types::Format::Clear:
+            wid_text->textCursor().clearSelection();
+            return;
+
+        case Types::Format::SelectAll:
+            wid_text->selectAll();
+            return;
+
+        case Types::Format::Bold:
+            return;
+
+        case Types::Format::Italic:
+            wid_text->setFontItalic(!wid_text->fontItalic());
+            return;
+
+        case Types::Format::Underline:
+            wid_text->setFontUnderline(!wid_text->fontUnderline());
+            return;
+
+        case Types::Format::Left:
+            return;
+        case Types::Format::Center:
+            return;
+        case Types::Format::Right:
+            return;
+        case Types::Format::Fill:
+            return;
+    }
 }
 
 //--- protected methods ---
@@ -19,4 +76,9 @@ void TextWindow::changeEvent(QEvent *event)
         retranslateUi(this);
 
     QWidget::changeEvent(event);
+}
+
+void TextWindow::setupActions()
+{
+    // toolbar buttons
 }
--- sources/TextWindow.hxx
+++ sources/TextWindow.hxx
@@ -2,6 +2,7 @@
 
 #include <QWidget>
 #include "ui_TextWindow.h"
+#include "Types.hxx"
 
 class TextWindow : public QWidget, public Ui::TextWindow {
     Q_OBJECT
@@ -16,7 +17,11 @@ public:
     TextWindow &operator=(const TextWindow &rhs) = delete;
     TextWindow &operator=(TextWindow &&rhs) = delete;
 
+    //--- public methods ---
+    void formatText(const Types::Format format);
+
 protected:
     //--- protected methods ---
     virtual void changeEvent(QEvent *event) override final;
+    void setupActions();
 };
