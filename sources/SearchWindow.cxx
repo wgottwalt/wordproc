f893248bd4a144190713425f942ede4b47366788
Wilken 'Akiko' Gottwalt <12194808+wgottwalt@users.noreply.github.com>
2021-05-17 13:29:57 +0200

    added functionality to the SearchWindow class

--- sources/SearchWindow.cxx
+++ sources/SearchWindow.cxx
@@ -6,6 +6,8 @@ SearchWindow::SearchWindow(QWidget *parent)
 : QWidget(parent), Ui::SearchWindow()
 {
     setupUi(this);
+    setupActions();
+    lay_main->setSizeConstraint(QLayout::SetFixedSize);
 }
 
 SearchWindow::~SearchWindow()
@@ -20,4 +22,38 @@ void SearchWindow::changeEvent(QEvent *event)
         retranslateUi(this);
 
     QWidget::changeEvent(event);
+}
+
+void SearchWindow::setupActions()
+{
+    connect(btn_close, &QPushButton::clicked, this, &SearchWindow::close);
+    connect(btn_prev, &QPushButton::clicked, [&]()
+    {
+        QTextDocument::FindFlags flags = QTextDocument::FindBackward;
+
+        if (chk_case_sensitive->isChecked())
+            flags |= QTextDocument::FindCaseSensitively;
+        if (chk_whole_words->isChecked())
+            flags |= QTextDocument::FindWholeWords;
+
+        emit searchRequest(wid_lineedit->text(), flags, false);
+    });
+    connect(btn_next, &QPushButton::clicked, [&]()
+    {
+        QTextDocument::FindFlags flags;
+
+        if (chk_case_sensitive->isChecked())
+            flags |= QTextDocument::FindCaseSensitively;
+        if (chk_whole_words->isChecked())
+            flags |= QTextDocument::FindWholeWords;
+
+        emit searchRequest(wid_lineedit->text(), flags, false);
+    });
+}
+
+//--- public methods ---
+
+void SearchWindow::setSearchString(const QString &str)
+{
+    wid_lineedit->setText(str);
 }
--- sources/SearchWindow.hxx
+++ sources/SearchWindow.hxx
@@ -17,7 +17,16 @@ public:
     SearchWindow &operator=(const SearchWindow &rhs) = delete;
     SearchWindow &operator=(SearchWindow &&rhs) = delete;
 
+    //--- public methods ---
+    void setSearchString(const QString &str);
+    void setupActions();
+
 protected:
     //--- protected methods ---
     virtual void changeEvent(QEvent *event) override final;
+
+signals:
+    //--- Qt signals ---
+    void searchRequest(const QString &str, const QTextDocument::FindFlags flags,
+                       const bool use_regexp);
 };
